33a67da581fb38bee5afb18fc15e1648188bef99
Dave Hale <davejlhale@yahoo.co.uk>
2022-12-08 18:15:12 +0000

    init

--- src/app.js
+++ src/app.js
@@ -2,16 +2,25 @@ const db = require("./db/connection")
 const yargs = require("yargs");
 const Movie = require("./schemas/Movie");
 
-// * build the search query filter obj key:value pairs from a string (eg from yargs)
+// * build the search query filter obj with key:value pairs parsed from a string (eg from yargs)
 // user yarg string formatted as "key:value,key:value"
 const buildObjFromString = (argsString) => {
     console.log(`building filter Object from the string: "${argsString}"`)
+    
     let obj = {};
+
+    //get an array of all keys in our Movie schema
+    const props = Object.keys(Movie.schema.paths);
+    //split the string at each ,  into key:values then split those inturn at the :
     let buildQueryFrom = argsString.split(/,(?=[^,]+:)/).map(s => s.split(':'));
+    //map over the array of [ [key ,value] ... [key ,value]  ]
     buildQueryFrom.map((pair) => {
-        const props = Object.keys(Movie.schema.paths);
+        //for each key in schema 
         props.map((key) => {
-            if (pair[0] === key) { obj[pair[0]] = pair[1] }
+            //if pair[0] is a schema key 
+            if (pair[0] === key) { 
+                //add the key:value to our obj
+                obj[pair[0]] = pair[1] }
         })
     });
     return obj;
@@ -22,34 +31,51 @@ const crudHandler = async (args) => {
     db.connect();
     let queryFilter = {};
     let results = null;
+
+    //start update
     if (args.create) {
         try {
+            //if not supplied by yargs defualts from schema used
             const movie = new Movie({ title: args.title, actor: args.actor, director: args.director, rating: args.rating })
-            movie.save().catch((error) => console.log(error.message));
-            console.table(movie._doc)
+            //if mongoose cant save then schema / mongoose error caught
+            await movie.save().then(()=>console.log("adding document")).catch((error) => console.log(error.message));
+            //movie._doc is our schema document - ie fields
+            // console.table(movie._doc,)
         }
         catch (e) { console.log(e) }
-    }//end create
+    }
+    //end create
+
+    //start read - aka find documents based on our filter
     else if (args.query) {
         switch (args.query) {
             case "all":
                 queryFilter = {};
-                results = await Movie.findByFilterObj(queryFilter);
                 break;
             default:
+                //build our find filter from yargs input
                 queryFilter = buildObjFromString(args.query);
-                results = await Movie.findByFilterObj(queryFilter);
                 break;
         }
+        //findByFilterObj is a static function defined with the schema in movie.js 
+        results = await Movie.findByFilterObj(queryFilter);
         console.log(`Found ${results.length} documents`);
+        //displayTable is statis schema function defined with the schema in movie.js
         Movie.displayTable(results);
-    } //end query
+    } 
+    //end query 
+    //end read
+
+    //start update
     else if (args.update) {
         queryFilter = buildObjFromString(args.update)
         console.log("query", queryFilter)
+        //if the query object we built from the yargs input had valid keys added
         if (Object.keys(queryFilter).length !== 0) {
+            //findByFilterObj is a static function defined with the schema in movie.js 
             results = await Movie.findByFilterObj(queryFilter)
-            console.log(results.length)
+            console.log(`Found ${results.length} objects matching query string to update`)
+            //for each document matching our query
             results.map((movie) => {
                 if (args.title) {
                     movie.title = args.title;
@@ -64,32 +90,39 @@ const crudHandler = async (args) => {
                     movie.rating = args.rating;
                 }
                 movie.updatedAt = Date.now();
+                //save our changed values to allow mongoose built in validations
+                //if validation or reason fails to save catch the error
                 movie.save().then(function (v) {
-                    console.log("Document updated");
+                    console.log("Updating Document");
                     console.table(v._doc)
                 }).catch((error) => console.log(error.message));
             })
         }
-
-
     }//end update
+
+    //start delete
     else if (args.delete) {
-        console.log(args.delete)
+        /*switch on yarg cli input
+        --delete all 
+        or 
+        --delete "key:value,key/value" */
         switch (args.delete) {
-            case "all": queryFilter = {};
-                results = await Movie.findByFilterObj(queryFilter)
+            case "all": 
+                queryFilter = {};
                 break;
             default:
                 queryFilter = buildObjFromString(args.delete)
-                results = await Movie.findByFilterObj(queryFilter)
                 break;
         }
+        results = await Movie.findByFilterObj(queryFilter)
         console.log(`Deleting ${results.length} results`);
         results.map((movie) => {
             movie.delete();
         })
+    } 
+    //end delete
 
-    } //end delete
+
     else {
         console.log("Command not recognized");
     }
@@ -97,7 +130,7 @@ const crudHandler = async (args) => {
     setTimeout(() => {
         db.disconnect();
     }, 1000);
-    
+
 }
 
 
